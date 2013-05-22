4bf19b16383341cf9df42d7ca4f296307c2c30d2
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-05-22 23:50:02 +0100

    Basic full rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassyCate::Application.config.secret_token = '664d2faa15d340e611e91ff2c4e9c09d0af5aae3136dbac03628d202c9de577f21eebc9a2351d3139aea12d252a774bef12406877b7480fb4c9f4672d848572f'
