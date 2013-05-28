82b3bdf6ff608512da6c751c4ee40897fd44c36b
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-05-28 04:35:32 +0100

    Moved to user/pass from email/pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,25 +3,21 @@ class SessionsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def create
-    verified = login()
-    puts '*** CREATING NEW SESSION ***'
-    user = User.authenticate(params[:user][:login], params[:user][:password])
     # correct credentials
     if login()
+      login, pass = params[:user][:login], params[:user][:password]
+      user = User.authenticate(login,pass)
       if !user
         user = User.new(params[:user])
         user.save()
       end
-      # set the current session user
-      session[:user_login] = user.login
       # generate random token for key
-      random_token = SecureRandom.urlsafe_base64
+      token = SecureRandom.urlsafe_base64
       # push into the cookie the random token that is
       # about to expire
-      cookies.permanent.signed[:cipher_key] = random_token
-      password = params[:user][:password]
-      key = Cate::Connection.generate_session_pass user, password, random_token
-      session[:session_pass] = key
+      cookies.permanent.signed[:cipher_key] = token
+      key = Cate::Connection.generate_session_pass user, pass, token
+      session[:user_login], session[:session_pass] = login, key
       redirect_to root_url
     else
       redirect_to root_url, :alert => 'Invalid credentials, please try again'
@@ -29,7 +25,7 @@ class SessionsController < ApplicationController
   end
 
   def login
-    params[:user][:login] = params[:user][:email].split('@')[0]
+    params[:user][:email] = params[:user][:login] + '@ic.ac.uk'
     cate = Cate::Connection.new params[:user][:login]
     verified = cate.verify_login(params[:user][:password])
     cate.destroy()
